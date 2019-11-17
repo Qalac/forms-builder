0ac24453b74d1d8a5197afc53bba26c78e2d2735
Qalac <officialqalac@gmail.com>
2019-11-17 02:50:01 +0000

    made changes to settings file

--- Eddie/settings.py
+++ Eddie/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,12 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'lgq7goho@0k9!a@+%pkde9zd2hptdp=noa!n-$9#-@3j#3tlaf'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'lgq7goho@0k9!a@+%pkde9zd2hptdp=noa!n-$9#-@3j#3tlaf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ['DEBUG']
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'form-build.herokuapp.com']
 
